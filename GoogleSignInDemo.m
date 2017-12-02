8992479675346cbc8136ee84daca96f489bc8413
hiren1995 <kadamhiren.sigma@gmail.com>
2017-12-02 15:31:25 +0530

    Initial commit

---
+++ GoogleSignInDemo.m
@@ -0,0 +1,9 @@
+//
+//  GoogleSignInDemo.m
+//  GoogleSignInDemo
+//
+//  Created by APPLE MAC MINI on 28/11/17.
+//  Copyright © 2017 APPLE MAC MINI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ ViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ViewController.swift
+//  GoogleSignInDemo
+//
+//  Created by APPLE MAC MINI on 28/11/17.
+//  Copyright © 2017 APPLE MAC MINI. All rights reserved.
+//
+
+import UIKit
+
+//AppDelegate should implement the GIDSignInUIDelegate Protocol Of Google SDK
+
+class ViewController: UIViewController,GIDSignInUIDelegate {
+
+    
+    @IBOutlet weak var signInButton: GIDSignInButton!
+    
+    @IBOutlet weak var profilePic: UIImageView!
+    
+    @IBOutlet weak var lblName: UILabel!
+    
+   
+    @IBOutlet weak var lblEmail: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        GIDSignIn.sharedInstance().uiDelegate = self
+      
+        //GIDSignIn.sharedInstance().signInSilently()
+        
+        loadDetails()
+       
+        
+    }
+    
+    func loadDetails()
+    {
+            if(ProfileImg == nil)
+            {
+                profilePic.backgroundColor = UIColor.gray
+            }
+            else{
+                
+                let imgData = NSData(contentsOf: ProfileImg!)
+                profilePic.image = UIImage(data: imgData! as Data)
+            }
+        
+            if(UserName == nil)
+            {
+                lblName.text = "Please sign in first"
+            }
+            else{
+                lblName.text = UserName
+            }
+        
+            if(Email == nil)
+            {
+                lblEmail.text = "Please sign in first"
+            }
+            else
+            {
+                lblEmail.text = Email
+            }
+      
+    }
+
+    @IBAction func btnLogout(_ sender: Any) {
+        
+        GIDSignIn.sharedInstance().signOut()
+        
+        profilePic.image = nil
+        profilePic.backgroundColor = UIColor.gray
+        lblName.text = "Please sign in first"
+        lblEmail.text = "Please sign in first"
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
